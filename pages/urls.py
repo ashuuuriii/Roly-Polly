81da604067d964883cbb605e61f038cac6ea3df6
Ashley <ashleypak71@yahoo.com>
2022-05-28 01:58:48 +0900

    Added urls to pages app TemplateView

--- pages/urls.py
+++ pages/urls.py
@@ -1,4 +1,9 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, AboutView, FaqView
 
-urlpatterns = [path("", HomePageView.as_view(), name="home")]
+
+urlpatterns = [
+    path("about", AboutView.as_view(), name="about"),
+    path("faq", FaqView.as_view(), name="faq"),
+    path("", HomePageView.as_view(), name="home"),
+]
