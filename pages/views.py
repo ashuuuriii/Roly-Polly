50a17ae2ede1d80380b8f5fd84a48ac199eb49a5
Ashley <ashleypak71@yahoo.com>
2022-05-28 01:55:34 +0900

    Added AboutView and FaqView

--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,11 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = "index.html"
+
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+
+class FaqView(TemplateView):
+    template_name = "faq.html"
