43e1dc780347dc58deb747298c03a80bca9f8cb1
Ashley <ashleypak71@yahoo.com>
2022-05-30 20:52:16 +0900

    Added accounts app

--- RolyPolly/settings.py
+++ RolyPolly/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # local apps
-    "pages.apps.PagesConfig",    
+    "pages.apps.PagesConfig",
+    "accounts.apps.AccountsConfig"
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "accounts.CustomUser"
