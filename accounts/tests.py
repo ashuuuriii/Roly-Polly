e17531c4bc1a5075e0d1293fbeb4b8155b4a273e
Ashley <ashleypak71@yahoo.com>
2022-06-06 23:59:39 +0900

    Added page location tests for accounts app

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class SignupPageTests(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/accounts/signup/", follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_signup_view_name(self):
+        response = self.client.get(reverse("account_signup"), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "account/signup.html")
+
+
+class LogInPageTests(TestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/accounts/login/", follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_view_name(self):
+        response = self.client.get(reverse("account_login"), follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "account/login.html")
