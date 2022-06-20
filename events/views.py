13293682129f0465b3227a12aa9de38262b4f614
Ashley <ashleypak71@yahoo.com>
2022-06-21 01:15:17 +0900

    Added event creation view as a function view

--- events/views.py
+++ events/views.py
@@ -1,9 +1,30 @@
-from django.views.generic.edit import FormView
+from django.shortcuts import render
 
-from .forms import NewEventForm
+from .forms import NewEventForm, ChoiceFormset
+from .models import Choice
 
 
-class NewEventFormView(FormView):
+def new_event_view(request):
     template_name = "new_event.html"
-    form_class = NewEventForm
-    # success_url = '/saved/'yy
+    if request.method == "GET":
+        event_form = NewEventForm(request.GET or None)
+        choice_formset = ChoiceFormset(queryset=Choice.objects.none())
+    elif request.method == "POST":
+        event_form = NewEventForm(request.POST)
+        choice_formset = ChoiceFormset(request.POST)
+        if event_form.is_valid() and choice_formset.is_valid():
+            event = event_form.save(commit=False)
+            event.user_id = request.user
+            event.save()
+            for formset in choice_formset:
+                choice = formset.save(commit=False)
+                choice.event_id = event
+                choice.save()
+            choice_formset.save()
+        # TODO: redirect to success page
+
+    return render(
+        request,
+        template_name,
+        {"event_form": event_form, "choice_form": choice_formset},
+    )
