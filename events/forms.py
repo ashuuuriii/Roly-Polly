7711f346c70eb98fca081ff65c8d9027e363ab70
Ashley <ashleypak71@yahoo.com>
2022-07-01 00:18:19 +0900

    Added password hashing to NewEventForm save method

--- events/forms.py
+++ events/forms.py
@@ -1,24 +1,32 @@
-from django.forms import ModelForm, modelformset_factory, DateTimeField, PasswordInput
+from django import forms
+from django.contrib.auth.hashers import make_password
 
 from .models import Event, Choice
 
 
-class NewEventForm(ModelForm):
+class NewEventForm(forms.ModelForm):
     class Meta:
         model = Event
         exclude = ["user_id"]
         widgets = {
-            "password": PasswordInput(),
+            "password": forms.PasswordInput(),
         }
 
+    def save(self, commit=True):
+        event = super(NewEventForm, self).save(commit=False)
+        event.password = make_password(self.cleaned_data["password"])
+        if commit:
+            event.save()
+        return event
 
-class ChoiceForm(ModelForm):
+
+class ChoiceForm(forms.ModelForm):
     # TODO: add validation to prevent users from inputting past dates or duplicates
-    time_from = DateTimeField(input_formats=["%d/%m/%Y"])
-    time_to = DateTimeField(input_formats=["%d/%m/%Y"])
+    time_from = forms.DateTimeField(input_formats=["%d/%m/%Y"])
+    time_to = forms.DateTimeField(input_formats=["%d/%m/%Y"])
 
 
 # use formset factory to create dynamic fields
-ChoiceFormset = modelformset_factory(
+ChoiceFormset = forms.modelformset_factory(
     Choice, form=ChoiceForm, fields=["time_from", "time_to"], extra=1, can_delete=True
 )
