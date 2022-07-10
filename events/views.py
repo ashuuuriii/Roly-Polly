9760882b8ce8e28c9f40bd29178894f73211ef50
ashley <ashleypak71@yahoo.com>
2022-07-10 15:19:14 +0900

    Updated vote view to support pw protection

--- events/views.py
+++ events/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import NewEventForm, ChoiceFormset, UnlockVoteForm
-from .models import Choice
+from .models import Choice, Event
 
 
 @login_required
@@ -50,9 +50,72 @@ class NewEventSuccessView(LoginRequiredMixin, TemplateView):
         return context
 
 
-class EventVoteView(TemplateView):
+class EventVoteView(DetailView):
     template_name = "vote.html"
+    model = Event
+    slug_url_kwarg = "uuid_slug"
+    slug_field = "access_link"
+
+    def get(self, request, *args, **kwargs):
+        # makes the uuid slug available within context
+        self.object = self.get_object()
+        context = self.get_context_data(object=self.object, **kwargs)
+        return self.render_to_response(context)
+
+    def render_to_response(self, context, **response_kwargs):
+        """
+        checks if the user is the user creator, has already entered the password
+        or if the event is password protected.
+        """
+        uuid = context.get("uuid_slug")
+        password_protect = context.get("event").password_protect
+        event_user = str(context.get("event").user_id)
+        current_user = self.request.user.email if self.request.user.id else None
+
+        if (
+            self.request.session.get(f"unlock-{uuid}")
+            or not password_protect
+            or (event_user == current_user)
+        ):
+            return super().render_to_response(context, **response_kwargs)
+        else:
+            return redirect("unlock", uuid_slug=uuid)
 
 
-class UnlockVoteView(TemplateView):
+class UnlockVoteView(FormView):
     template_name = "unlock_vote.html"
+    form_class = UnlockVoteForm
+
+    def get(self, request, *args, **kwargs):
+        # makes the uuid slug available within context
+        return self.render_to_response(self.get_context_data(**kwargs))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["uuid_slug"] = kwargs.get("uuid_slug")
+        context["event_data"] = Event.objects.get(access_link=context["uuid_slug"])
+        return context
+
+    def render_to_response(self, context, **response_kwargs):
+        uuid = context.get("uuid_slug")
+        event_user = str(context.get("event_data").user_id)
+        password_protect = context.get("event_data").password_protect
+        current_user = self.request.user.email if self.request.user.id else None
+
+        if (
+            self.request.session.get(f"unlock-{uuid}")
+            or not password_protect
+            or (event_user == current_user)
+        ):
+            return redirect("vote", uuid_slug=uuid)
+        else:
+            return super().render_to_response(context, **response_kwargs)
+
+    def form_valid(self, form):
+        """
+        password checking is implemented within the form"s clean_password
+        method.
+        """
+        uuid_slug = form.data.get("event_id")
+        self.request.session[f"unlock-{uuid_slug}"] = True
+        return redirect("vote", uuid_slug=uuid_slug)
