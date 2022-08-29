e520dc50539b801f824bef09041a13a606f4b4df
ashley <ashleypak71@yahoo.com>
2022-08-29 19:40:20 +0900

    Changed login redirect url from index to dashboard view

--- accounts/adapter.py
+++ accounts/adapter.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from allauth.account.adapter import DefaultAccountAdapter
+from django.urls import reverse
 
 from .tasks import send_email_async
 
@@ -8,3 +9,6 @@ class CustomAccountAdapter(DefaultAccountAdapter):
     def send_mail(self, template_prefix, email, context):
         msg = self.render_mail(template_prefix, email, context)
         send_email_async.delay(msg)
+
+    def get_login_redirect_url(self, request):
+        return reverse('dashboard')
